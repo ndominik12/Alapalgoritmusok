f687ef3f3c339860d524ad057032d89d276a6a67
ndominik12 <ndomi560@gmail.com>
2025-11-05 11:34:39 +0100

    elso feladat is ready

---
+++ Alapalgoritmusok/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alapalgoritmusok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SzamFeldolgozo
+{
+    private List<int> szamok;
+
+    public SzamFeldolgozo(List<int> szamok)
+    {
+        this.szamok = new List<int>(szamok);
+    }
+
+    public List<int> Lista()
+    {
+        return new List<int>(szamok);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var feldolgozo = new SzamFeldolgozo(new List<int> { 1, 2, 3, 4, 5 });
+        Console.WriteLine("A SzamFeldolgozo példány sikeresen létrejött.");
+        Console.WriteLine("A lista tartalma: " + string.Join(", ", feldolgozo.Lista()));
+    }
+}
