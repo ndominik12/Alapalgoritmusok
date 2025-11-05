5171fe6d2ec46c075d1ed590911f6d4f093c0784
ndominik12 <ndomi560@gmail.com>
2025-11-05 11:45:59 +0100

    feladat 3 is ready

--- Alapalgoritmusok/Program.cs
+++ Alapalgoritmusok/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class SzamFeldolgozo
 {
@@ -8,6 +9,26 @@ public class SzamFeldolgozo
     public SzamFeldolgozo(List<int> szamok)
     {
         this.szamok = new List<int>(szamok);
+    }
+
+    public int Osszeg()
+    {
+        return szamok.Sum();
+    }
+
+    public double Atlag()
+    {
+        return szamok.Count == 0 ? 0 : szamok.Average();
+    }
+
+    public bool Tartalmazza(int cel)
+    {
+        foreach (int szam in szamok)
+        {
+            if (szam == cel)
+                return true;
+        }
+        return false;
     }
 
     public List<int> Lista()
@@ -21,7 +42,9 @@ class Program
     static void Main()
     {
         var feldolgozo = new SzamFeldolgozo(new List<int> { 1, 2, 3, 4, 5 });
-        Console.WriteLine("A SzamFeldolgozo példány sikeresen létrejött.");
-        Console.WriteLine("A lista tartalma: " + string.Join(", ", feldolgozo.Lista()));
+        Console.WriteLine($"Összeg: {feldolgozo.Osszeg()}");
+        Console.WriteLine($"Átlag: {feldolgozo.Atlag()}");
+        Console.WriteLine($"Tartalmazza a 3-ast? {feldolgozo.Tartalmazza(3)}");
+        Console.WriteLine($"Tartalmazza a 9-est? {feldolgozo.Tartalmazza(9)}");
     }
 }
